b340622f495bfbe53052ff164d637c76a2d24d52
keco-mentario <83693253+keco-mentario@users.noreply.github.com>
2022-10-08 02:12:34 -0600

    Add files via upload

---
+++ payment.cpp
@@ -0,0 +1,5 @@
+#include "payment.h"
+
+payment::payment() {
+
+}
---
+++ payment.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "time.h"
+
+class payment {
+private:
+	time** entryTime;
+	time** exitTime;
+	double totalPayemnt;
+public:
+	payment();
+};
